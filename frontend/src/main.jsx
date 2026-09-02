9b7219cd38c2a15fc4fd6e323a1188e9d96d2da9
Rishika Khandelwal <212035609+Forgetallpasswords@users.noreply.github.com>
2026-09-02 17:23:00 +0530

    landing page started

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Landing from './pages/Landing/Landing.jsx'
+import Home from './pages/Home/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Landing/>
+        //TODO: add applayout and landinglayout and use them
+      },
+      {
+        path: "/home",
+        element: <Home/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={rotuer}/>
   </StrictMode>,
 )
